42d5151fa2791b3d3a0ec2d6d070993538709c31
Ryan Wittmers <rwittmers19@georgefox.edu>
2022-05-09 18:27:38 -0700

    continuing thank you tests

--- src/js/thankyou.js
+++ src/js/thankyou.js
@@ -1,8 +1,5 @@
 // After form submission, hide the contact form and display the thank you message div with class thank-you-message
-$document.getElementById('contact100-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-});
-$document.getElementById('contact-form').style.display = 'none';
+$document.getElementById('contact100-form').style.display = 'none';
 $document.getElementsByClassName('thank-you-message').remove.className = 'hidden';
 $document.getElementById('thank-you-message').style.display = 'flex';
 
